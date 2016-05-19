f58e27b2c259afdf3ede51cfccf5f6181c1be13e
mspark93 <mspark93@gmail.com>
2016-05-19 10:49:43 +0900

    Update vtkOpenGLRenderer.cxx
    memory leak !!!

--- Rendering/OpenGL2/vtkOpenGLRenderer.cxx
+++ Rendering/OpenGL2/vtkOpenGLRenderer.cxx
@@ -595,7 +595,8 @@ void vtkOpenGLRenderer::DonePick()
         dbPtr++;
         }
       }
-
+    delete pixBuffer;
+    delete depthBuffer;
     this->PickInfo->NumPicked = (unsigned int)this->PickInfo->PickValues.size();
 
     this->PickInfo->PickedId = 0;
